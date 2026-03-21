1fc962f6ad311b1fba5f55b822249ffff5a6cd1d
Xiaowen <liuxiaowencs@gmail.com>
2026-03-21 12:17:02 -0500

    Fixed an issue in extract_folder_prsm_info.sh

--- script/extract_folder_prsm_info.sh
+++ script/extract_folder_prsm_info.sh
@@ -4,9 +4,9 @@ DIR="$(dirname "$(realpath "$0")")"
 
 for f in "$DIR"/*.tsv; do
     basename="$(basename "$f")"
-    base="${basename%_toppic_prsm_single_replaced.tsv}"
+    base="${basename%_ms2_toppic_prsm_single_replaced.tsv}"
     project_id="${basename%%_*}"
-    out="${base}_ms2_toppic_info.tsv"
+    out="${base}_toppic_info.tsv"
     python3 prsm_preprocess.py $f $project_id --output $out
     echo "Processed: $(basename "$f") $project_id -> $(basename "$out")"
 done
