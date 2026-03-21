fa7df76e05de6f312305e3c7cbc8cdeaecd746ae
Xiaowen <liuxiaowencs@gmail.com>
2026-03-21 14:15:08 -0500

    Added extract_folder_proteoform_info.sh

---
+++ script/extract_folder_proteoform_info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR="$(dirname "$(realpath "$0")")"
+
+for f in "$DIR"/*.tsv; do
+    basename="$(basename "$f")"
+    base="${basename%_ms2_toppic_proteoform_single_replaced.tsv}"
+    project_id="${basename%%_*}"
+    out="${base}_toppic_proteoform_info.tsv"
+    python3 prsm_preprocess.py $f $project_id --output $out
+    echo "Processed: $(basename "$f") $project_id -> $(basename "$out")"
+done
+
+echo "Done."
