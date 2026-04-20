d43e59fe5ce3714093d38fd2d39c403d02ebb80e
xiaowen <liuxiaowencs@gmail.com>
2026-04-20 16:28:21 -0500

    Fixed a bug in merge_folder_spectrum_info.sh

--- script/merge_folder_spectrum_info.sh
+++ script/merge_folder_spectrum_info.sh
@@ -10,7 +10,7 @@ for msalign_fname in "$DIR"/*msalign_info.tsv; do
   toppic_fname="${msalign_base}_toppic_info.tsv"
   out="${msalign_base}_combined_info.tsv"
   echo "python3 merge_mzml_msalign_toppic_info.py $msalign_basename $feature_fname $toppic_fname toprepo_file_info_v1.2.0.tsv $out"
-  python3 merge_mzml_msalign_toppic_info.py $msalign_basename $feature_fname $toppic_fname $out
+  python3 merge_mzml_msalign_toppic_info.py $msalign_basename $feature_fname $toppic_fname toprepo_file_info_v1.2.0.tsv $out
   echo "Processed: $msalign_basename -> $(basename "$out")"
 done
 
